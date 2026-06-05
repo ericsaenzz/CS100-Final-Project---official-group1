b00d16ed63dd628662e61bc849a150aa7c9f61b8
jtate009 <jtate009@ucr.edu>
2026-06-05 09:08:05 -0700

    Update main.cpp to include menu for task scheduler

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 #include "UndoSystem.h"
 using namespace std;
 
+void showMenu() {
+    cout << "\n========== TASK SCHEDULER ==========\n";
+    cout << "1. Create Task\n";
+    cout << "2. View Tasks\n";
+    cout << "3. Edit Task\n";
+    cout << "4. Delete Task\n";
+    cout << "5. Manage Subtasks\n";
+    cout << "6. Mark Task Complete\n";
+    cout << "7. Sort Tasks\n";
+    cout << "8. View Overdue Tasks\n";
+    cout << "9. Generate Schedule\n";
+    cout << "10. Save Tasks\n";
+    cout << "11. Load Tasks\n";
+    cout << "12. Undo Last Action\n";
+    cout << "13. Exit\n";
+    cout << "Enter your choice: ";
+}
 int main(){
     
     return 0;
