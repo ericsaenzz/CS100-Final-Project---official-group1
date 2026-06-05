9047b016f51dcbf347c52dd64022dcdae286f6b8
sohamsanket <103902382+sohamsanket@users.noreply.github.com>
2026-06-05 15:08:45 -0700

    Update Scheduler.cpp

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -6,12 +6,14 @@ using namespace std;
 vector<Task> Scheduler::generateTodayAgenda(vector<Task> tasks) {
     vector<Task> agenda;
 
-    for (int i = 0; i < tasks.size(); i++) {
+    // Add only tasks that are not complete
+    for (size_t i = 0; i < tasks.size(); i++) {
         if (tasks[i].task_status != "complete") {
             agenda.push_back(tasks[i]);
         }
     }
 
+    // Sort by due date, then priority, then duration
     sort(agenda.begin(), agenda.end(), [](Task a, Task b) {
         if (a.task_dueDate != b.task_dueDate) {
             return a.task_dueDate < b.task_dueDate;
@@ -21,7 +23,7 @@ vector<Task> Scheduler::generateTodayAgenda(vector<Task> tasks) {
             return a.task_priority < b.task_priority;
         }
 
-        return a.task_estimatedTime < b.task_estimatedTime;
+        return a.task_duration < b.task_duration;
     });
 
     return agenda;
