0f6d40717f6e9f41d25f75386f1fe54c851258da
akama031-lab <akama031@ucr.edu>
2026-06-05 15:23:29 -0700

    Fix Scheduler.h file contents

--- include/Scheduler.h
+++ include/Scheduler.h
@@ -1,4 +1,3 @@
-cat > include/Scheduler.h << 'EOF'
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 #include "Task.h"
@@ -14,5 +13,19 @@ public:
     vector<Task> sortByStatus(vector<Task> tasks);
     vector<Task> getOverdueTasks(vector<Task> tasks);
 };
+#endif#ifndef SCHEDULER_H
+#define SCHEDULER_H
+#include "Task.h"
+#include <vector>
+using namespace std;
+class Scheduler {
+public:
+    vector<Task> generateTodayAgenda(vector<Task> tasks);
+    vector<Task> generateSchedule(vector<Task> tasks);
+    vector<Task> sortByDeadline(vector<Task> tasks);
+    vector<Task> sortByPriority(vector<Task> tasks);
+    vector<Task> sortByTime(vector<Task> tasks);
+    vector<Task> sortByStatus(vector<Task> tasks);
+    vector<Task> getOverdueTasks(vector<Task> tasks);
+};
 #endif
-EOF
